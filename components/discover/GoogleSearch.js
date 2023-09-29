5b6bdc88ce7e1b35c3ab9af7607358d42b818347
Lambertn33 <lambertgithub@gmail.com>
2023-09-29 10:35:11 +0200

    feat: search a place

---
+++ components/discover/GoogleSearch.js
@@ -0,0 +1,28 @@
+import { ScrollView } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import React from "react";
+
+const GoogleSearch = () => {
+  return (
+    <ScrollView
+      keyboardShouldPersistTaps="handled"
+      contentContainerStyle={{ flexGrow: 1 }}
+    >
+      <GooglePlacesAutocomplete
+        GooglePlacesSearchQuery={{ fields: "geometry" }}
+        disableScroll={true}
+        placeholder="Search"
+        fetchDetails={true}
+        onPress={(data, details = null) => {
+          console.log('details', details.geometry.viewport);
+        }}
+        query={{
+          key: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
+          language: "en",
+        }}
+      />
+    </ScrollView>
+  );
+};
+
+export default GoogleSearch;
--- screens/DiscoverScreen.js
+++ screens/DiscoverScreen.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import DiscoverHeader from "../components/discover/DiscoverHeader";
+import GoogleSearch from "../components/discover/GoogleSearch";
 
 const DiscoverScreen = () => {
   return (
     <View style={styles.container}>
       <DiscoverHeader />
+      <GoogleSearch />
     </View>
   );
 };
